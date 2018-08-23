28e08e8bdc1803fa0e499a150908ded9b9e37cd5
Irina Shestak <shestak.irina@gmail.com>
2018-08-23 17:12:54 +0200

    add wasm-bidngen to generate a wasm pack

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 #![cfg_attr(feature = "nightly", deny(missing_docs))]
 #![cfg_attr(feature = "nightly", feature(external_doc))]
 #![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
+#![cfg_attr(feature = "nightly", deny(unsafe_code))]
 #![cfg_attr(test, deny(warnings))]
 
 #[macro_use]
 extern crate failure;
 
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
 mod error;
 
 pub use error::{Error, ErrorKind, Result};
+
+#[wasm_bindgen]
+pub fn parses(schema: &str) { 
+}
