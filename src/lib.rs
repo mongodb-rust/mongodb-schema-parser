89700571fd297d4064550de0065ac0d0e16b5769
lrlna <shestak.irina@gmail.com>
2018-11-05 16:11:02 +0100

    rm dead code

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,6 @@ use std::string::String;
 mod error;
 pub use error::{Error, ErrorKind, Result};
 
-mod schema;
-use schema::DocumentKind;
-use schema::Field;
-use schema::MongoDBSchema;
-use schema::PrimitiveType;
-
 fn add_field_schema_to_document(doc: &mut Document, value: Bson) {
   let value_type = match value {
     Bson::FloatingPoint(_) | Bson::I32(_) | Bson::I64(_) => "Number",
@@ -58,72 +52,6 @@ fn generate_schema_from_document(doc: Document) -> Document {
     // NOTE: This will be incorrect if the number of fields is greater than i64::MAX
     "count": count as i64,
     "fields": fields
-  }
-}
-
-pub fn parser() -> MongoDBSchema {
-  let mut values_vec = Vec::new();
-  values_vec.push(1);
-
-  let primitive_type = PrimitiveType {
-    name: String::from("Number"),
-    path: String::from("path"),
-    count: 1,
-    probability: 0.75,
-    unique: 1,
-    has_duplicates: false,
-    values: values_vec,
-  };
-
-  let primitive_type = DocumentKind::PrimitiveType(primitive_type);
-
-  let mut types_vec = Vec::new();
-  types_vec.push(primitive_type);
-
-  let field = Field {
-    name: String::from("_id"),
-    path: String::from("path"),
-    count: 1,
-    field_type: String::from("Number"),
-    probability: 0.75,
-    has_duplicates: false,
-    types: types_vec,
-  };
-
-  let mut field_vec = Vec::new();
-  field_vec.push(field);
-
-  let mongodb_schema = MongoDBSchema {
-    count: 4,
-    fields: field_vec,
-  };
-
-  mongodb_schema
-}
-
-#[cfg(test)]
-mod tests {
-  // if bringing in something from above scope: 'use'
-  // use super::{name_of_function_above};
-  use super::*;
-
-  #[test]
-  fn can_generate_schema() {
-    let data = r#"{
-      "github": {
-        "name": "Irina",
-        "handle": "lrlna"
-      },
-      "foo": 4
-    }"#;
-
-    let v: Value = serde_json::from_str(data)?;
-
-    let mongodb_schema = parser();
-    let count: usize = 4;
-    let field_name = String::from("_id");
-    assert_eq!(&mongodb_schema.count, &count);
-    assert_eq!(&mongodb_schema.fields[0].name, &field_name);
   }
 }
 
